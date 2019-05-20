1cb65d94d41a9b3572d3bcf4be9653bfa26bc05d
srad <ssedighi@posteo.de>
2019-05-20 22:13:43 +0200

    Clean up and minor refactoring.
    * Remove: Unused npm package.
     * Gets also rid of outdated package-lock.json which shows warnings on github.
    * Change: Dropping unused package form express + logging http requests in dev mode.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
+// |========================================================|
+// | This file only configures the express app instance     |
+// | and is imported by /bin/www                            |
+// |========================================================|
+
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const lessMiddleware = require("less-middleware");
-// Request logger
-// const morgan = require("morgan");
 const cors = require("cors");
 const timeout = require("connect-timeout");
 const fs = require("fs");
@@ -16,8 +18,6 @@ const graphQLAPI = require("./routes/api/v1/graphql");
 const apiCala = require("./routes/api/cala");
 const apiDashboard = require("./routes/api/dashboard");
 
-const app = express();
-
 logger.info(`
 ================================================================
 Running server, current environment: ${process.env.NODE_ENV} on port ${process.env.PORT}
@@ -27,42 +27,48 @@ Running server, current environment: ${process.env.NODE_ENV} on port ${process.e
 // |========================================================|
 // | Setup                                                  |
 // |========================================================|
+const app = express();
 
 if (!fs.existsSync("public/uploads")) {
   fs.mkdirSync("public/uploads", {recursive: true});
 }
 
-// |========================================================|
-// | Express Setup                                          |
-// |========================================================|
-
-function haltOnTimeOut(req, res, next) {
-  if (!req.timedout) {
-    next();
-  }
+if (app.get("env") === "development") {
+  const morgan = require("morgan");
+  app.use(morgan("dev"));
 }
 
 app.use(timeout("60m"));
-app.use(haltOnTimeOut);
-//app.use(morgan("dev"));
+app.use(function haltOnTimeout(req, res, next) {
+  if (!req.timedout) {
+    next();
+  }
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(apiMiddleware);
+
+// |========================================================|
+// | Routes                                                 |
+// |========================================================|
 
 app.use("/graphql/v1", graphQLAPI);
 // File uploads
 app.use("/api/cala", apiCala);
 app.use("/api/dashboard", apiDashboard);
 
-// Route handling. Only the upload folder is served statically.
-// All other static resource are compile by vue when they are included/required in components.
+// Only the upload folder is served statically beside the index.html
 app.get('/uploads/*', (req, res) => res.sendFile(__dirname + "/public" + req.url));
+// All other static resource are compile by vue when they are included/required in components.
 app.get('*', (req, res) => res.sendFile(__dirname + "/public/index.html"));
+
+// |========================================================|
+// | Error handling                                         |
+// |========================================================|
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
