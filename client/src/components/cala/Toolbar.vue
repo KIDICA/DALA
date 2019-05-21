6e9ffdc649d9c76743ac39b59257c3d9863326c1
srad <ssedighi@posteo.de>
2019-05-21 12:11:20 +0200

    Adding bottom toolbar component #28

---
+++ client/src/components/cala/Toolbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fixed-bottom w-100 bg-light pt-2 pl-1 pr-1 pb-1 text-center align-middle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "cala-toolbar"
+  }
+</script>
+
+<style scoped>
+
+</style>
