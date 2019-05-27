dd107ea6d496a094d3960c43181ecc2da648d105
srad <ssedighi@posteo.de>
2019-05-28 01:42:45 +0200

    Fix: Making sure performance is drawn properly on Hub.

--- client/src/components/cala/Predictor.vue
+++ client/src/components/cala/Predictor.vue
@@ -3,28 +3,23 @@
     <cala-busy ref="busy"></cala-busy>
 
     <div class="row no-gutters">
-      <div class="col  mr-2">
-        <div class="card h-100 border-0">
-          <div class="card-header text-primary bg-transparent pt-0 pr-0 pl-0 border-0 pb-2">
-            <span class="text-uppercase h4 font-weight-bold">Active Learning Session</span>
-          </div>
-          <div class="card-body p-0">
-            <img ref="image" class="img-thumbnail shadow-sm h-100 cover-image" v-bind:src="cover">
-          </div>
-        </div>
-      </div>
-
       <div class="col mr-2">
         <div class="card bg-light h-100 border-light">
           <div class="card-header font-weight-bold text-white bg-light border-0">
-            <span class="text-uppercase h4">Overall Stats</span>
+            <span class="text-uppercase h4 text-secondary">Overall Stats</span>
           </div>
           <div class="card-body p-3">
             <div class="row">
               <div class="col">
                 <small class="text-uppercase">Images Uploaded:</small>
-                <button class="btn-outline-primary border-2 bg-white btn-lg btn-block text-center">
+                <button class="btn-outline-primary border-2 btn-lg btn btn-block bg-white">
                   <span class="font-weight-bold">{{imageCount}}</span>
+                </button>
+              </div>
+              <div class="col">
+                <small class="text-uppercase">Unlabeled</small>
+                <button class="btn-dark border-2 btn btn-lg btn-block border-white text-white">
+                  <span class="font-weight-bold">{{count.unlabeled}}</span>
                 </button>
               </div>
             </div>
@@ -32,7 +27,7 @@
             <div class="row mt-2" v-for="tag in tags" :key="tag.id">
               <div class="col">
                 <small class="text-uppercase">{{tag.name}}</small>
-                <button :key="tag.id" class="border-2 btn btn-block btn-lg border-white text-white" v-bind:class="tag.className">
+                <button :key="tag.id" class="btn-lg border-2 btn btn-block  border-white text-white" v-bind:class="tag.className">
                   {{tag.imageCount}}
                 </button>
               </div>
@@ -44,43 +39,67 @@
       <div class="col">
         <div class="card bg-light h-100 border-light">
           <div class="card-header font-weight-bold text-white bg-light border-0">
-            <span class="text-uppercase h4">Average Precision</span>
+            <span class="text-uppercase h4 text-secondary">Average Precision</span>
           </div>
-          <div class="card-body text-center">
+          <div class="card-body text-center pt-4">
             <cala-pie-chart ref="performance"></cala-pie-chart>
             <span class="text-secondary font-weight-bolder display-4">
-              {{(averagePrecision*100).toFixed(2)}}%
+              {{(ap*100).toFixed(2)}}%
             </span>
           </div>
         </div>
       </div>
-
     </div>
 
-    <div class="row mt-4">
+    <div class="row mt-3">
       <div class="col">
-        <ul class="list-inline float-right">
-          <li class="list-inline-item mr-5" v-for="tag in tags" :key="tag.id">
-            <span class="badge p-2 legend mr-1" style="border-radius: 2em" v-bind:class="tag.className">&nbsp;</span>
-            <span style="font-size: 1.3em;" class="font-weight-bolder">{{tag.name}}</span>
-          </li>
-        </ul>
+        <span class="text-uppercase h4 font-weight-bold text-primary">Active Learning Session</span>
       </div>
     </div>
 
-    <div class="row p-0 mt-2" v-bind:style="{height: chartHeight}">
-      <div class="col">
-
-        <div class="card h-100">
-          <div class="card-body p-0">
-            <cala-line-chart ref="chart"></cala-line-chart>
-            <div class="btn-group" style="position: absolute; right:1em; bottom:1em;">
-              <cala-upload url="/api/dashboard/upload" v-on:uploaded="update"></cala-upload>
-              <button @click="train" class="btn btn-outline-primary btn-lg bg-white">Retrain</button>
+    <div class="row no-gutters mt-3" v-bind:style="{height: chartHeight}">
+      <div class="col-3  mr-2">
+        <div class="row">
+          <div class="col">
+            <div class="card h-100 border-0">
+              <div class="card-body p-0 h-100">
+                <div class="row">
+                  <div class="col">
+                    <img ref="image" class="h-100 shadow-sm mb-3 img-thumbnail shadow-sm cover-image" v-bind:src="cover"/>
+                  </div>
+                </div>
+                <div class="row mt-3">
+                  <div class="col">
+                    <cala-upload button-class="btn-block btn-lg btn-outline-primary bg-white" url="/api/dashboard/upload" v-on:uploaded="update"></cala-upload>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <button @click="train" class="mt-3 btn-lg btn btn-block btn-secondary">Retrain</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+      </div>
 
+      <div class="col">
+        <div class="card h-100">
+          <div class="card-body p-0">
+            <cala-line-chart ref="chart"></cala-line-chart>
+
+            <div class="position-absolute bg-white rounded-left rounded-right text-center"
+                 style="bottom: 0; left: 40%; border-top-left-radius: .8em !important;border-top-right-radius: .8em !important; opacity: .9">
+              <ul class="list-inline m-0 p-1">
+                <li class="list-inline-item mr-2 ml-2 pb-0" v-for="tag in tags" :key="tag.id">
+                  <span class="badge p-2 legend mr-3" style="border-radius: 2em" v-bind:class="tag.className">&nbsp;</span>
+                  <span style="font-size: 1.3em;" class="font-weight-bolder">{{tag.name}}</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -108,14 +127,19 @@
         busy: false,
         predictions: [],
         chartHeight: "5em",
-        averagePrecision: 0
+        averagePrecision: 0,
       };
     },
     computed: {
       imageCount: {
         get() {
           return this.$store.state.imageCount;
-        }
+        },
+      },
+      count: {
+        get() {
+          return this.$store.state.count;
+        },
       },
       tags: {
         get() {
@@ -126,11 +150,19 @@
             tag.className = classNames[index % count];
             return tag;
           });
-        }
+        },
       },
       hasIterations: {
         get() {
           return this.$store.state.hasIterations;
+        },
+        set(val) {
+          this.$store.state.hasIterations = val;
+        }
+      },
+      ap: {
+        get() {
+          return this.$store.state.performance.averagePrecision;
         }
       }
     },
@@ -140,7 +172,7 @@
       },
       hasIterations(val) {
         this.update();
-      }
+      },
     },
     methods: {
       train() {
@@ -151,6 +183,7 @@
         }`)
           .then(result => {
             this.$log.debug(result);
+            this.hasIterations = true;
             this.update();
             this.busy = false;
           })
@@ -160,37 +193,13 @@
           });
       },
       resizeChart() {
-        this.chartHeight = (document.body.clientHeight - 515) + "px";
+        this.chartHeight = (document.body.clientHeight - 490) + "px";
       },
       performance() {
-        this.busy = true;
-        this.$query(`
-          {
-            performance {
-              precision
-              recall
-              averagePrecision
-
-              perTagPerformance {
-                id
-                name
-                precision
-                recall
-                averagePrecision
-              }
-            }
-          }
-        `).then(result => {
-          this.averagePrecision = result.performance.averagePrecision;
-          this.$refs.performance.data = {
-            series: [result.performance.averagePrecision * 100],
-            labels: ["Performance"]
-          };
-          this.busy = false;
-        }).catch(error => {
-          alert(error);
-          this.busy = false;
-        })
+        this.$refs.performance.data = {
+          series: [this.ap * 100],
+          labels: ["Performance"],
+        };
       },
       predict() {
         this.busy = true;
@@ -231,7 +240,7 @@
 
             this.$refs.chart.data = {
               series: series,
-              labels: new Array(series.length).fill(0).map((p, index) => "T-" + index)
+              labels: new Array(series.length).fill(0).map((p, index) => "T-" + index),
             };
 
             this.resizeChart();
@@ -246,15 +255,15 @@
       update() {
         if (this.hasIterations) {
           this.predict();
-          this.performance();
+          setTimeout(() => this.performance(), 1000);
         }
-      }
+      },
     },
     mounted: function() {
       this.update();
-      window.addEventListener('resize', this.resizeChart);
-    }
-  }
+      window.addEventListener("resize", this.resizeChart);
+    },
+  };
 </script>
 
 <style scoped>
