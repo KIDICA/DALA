0b541303734f9c794aff640150c6e9e7f1125ff2
srad <ssedighi@posteo.de>
2019-05-28 01:30:15 +0200

    Hide prediction dashboard in mobile view, keep only labeling section.

--- client/src/components/cala/Hub.vue
+++ client/src/components/cala/Hub.vue
@@ -3,7 +3,7 @@
 
     <div class="col-lg-4 mb-2">
       <div class="card border-primary" v-bind:style="{height: height}">
-        <div class="card-header gradient text-white p-2">Image Stream</div>
+        <div class="card-header gradient text-white p-2 h6">Image Stream</div>
         <div class="card-body p-0" style="overflow-y: scroll">
           <cala-imagelist ref="list" v-bind:remove-button="false"></cala-imagelist>
         </div>
@@ -11,8 +11,8 @@
     </div>
 
     <div class="col-lg ml-3">
-      <div class="card border-primary" v-bind:style="{height: height}">
-        <div class="card-header gradient text-white p-2">Prediction</div>
+      <div class="card border-primary d-none d-lg-block" v-bind:style="{height: height}">
+        <div class="card-header gradient text-white p-2 h6">Prediction</div>
         <div class="card-body p-3">
           <cala-dashboard></cala-dashboard>
         </div>
